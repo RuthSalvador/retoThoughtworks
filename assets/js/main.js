a31dd34c60fea3176cc57db3fc073aa7d186bf7d
Mishmore <michellemore28@gmail.com>
2017-03-13 16:31:25 -0500

    pestañas con js

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+var mostrarOcultar = function(divtexte) {
+//Guardo Ids
+  var text1 = document.getElementById('contenido-1');
+  var text2 = document.getElementById('contenido-2');
+  var text3 = document.getElementById('contenido-3');
+  var text4 = document.getElementById('contenido-4');
+
+  if (divtexte == 'contenido-1') {
+    text1.style.display = 'block';
+    text2.style.display = 'none';
+    text3.style.display = 'none';
+    text4.style.display = 'none';
+  }
+  else if (divtexte == 'contenido-2') {
+    text1.style.display = 'none';
+    text2.style.display = 'block';
+    text3.style.display = 'none';
+    text4.style.display = 'none';
+  }
+  else if (divtexte == 'contenido-3') {
+    text1.style.display = 'none';
+    text2.style.display = 'none';
+    text3.style.display = 'block';
+    text4.style.display = 'none';
+  }
+  else if (divtexte == 'contenido-4') {
+    text1.style.display = 'none';
+    text2.style.display = 'none';
+    text3.style.display = 'none';
+    text4.style.display = 'block';
+  }
+}
+
+var closeBubble = document.getElementsByName('close-bubble');
+closeBubble.addEventListener('click', function(e) {
+  e.preventDefault();
+document.getElementsByClassName('tooltiptext').style.visibility= "hidden";
+});
+
+var addResources = document.getElementById('specify-resources');
+addResources.addEventListener('click', function(e) {
+  e.preventDefault();
+  alert('msg');
+  document.getElementsByClassName('tooltiptext').style.visibility= "visible";
+});
